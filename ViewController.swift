4d10f9f4d4f851d4e79c51b435512b09afb9f560
“Vilde <vilde@vevatne.no>
2017-08-01 13:01:48 -0700

    Get info from weather api

--- ViewController.swift
+++ ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel: UILabel!
 
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBAction func getWeather(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "http://www.weather-forecast.com/locations/Boston/forecasts/latest")!
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print(error)
+            } else {
+                if let unwrappedData = data {
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    print(dataString)
+                }
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
