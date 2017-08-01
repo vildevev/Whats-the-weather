4effb6369c89c0e08152daae6904788602f51b64
“Vilde <vilde@vevatne.no>
2017-08-01 15:13:59 -0700

    Change url so that it searches according to user input

--- ViewController.swift
+++ ViewController.swift
@@ -15,29 +15,83 @@ class ViewController: UIViewController {
     @IBOutlet var cityTextField: UITextField!
     
     @IBAction func getWeather(_ sender: Any) {
-        
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "http://www.weather-forecast.com/locations/Boston/forecasts/latest")!
-        let request = NSMutableURLRequest(url: url)
-        
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {
-            data, response, error in
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
             
-            if error != nil {
-                print(error)
-            } else {
-                if let unwrappedData = data {
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                    print(dataString)
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                var message = ""
+                
+                if error != nil {
+                    
+                    print(error)
+                    
+                } else {
+                    
+                    if let unwrappedData = data {
+                        
+                        
+                        
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        
+                        var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                        
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                            
+                            if contentArray.count > 1 {
+                                
+                                stringSeparator = "</span>"
+                                
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                                
+                                if newContentArray.count > 1 {
+                                    
+                                    message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                    
+                                    print(message)
+                                    
+                                }
+                                
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    
                 }
+                
+                if message == "" {
+                    
+                    message = "The weather there couldn't be found. Please try again."
+                    
+                }
+                
+                DispatchQueue.main.sync(execute: {
+                    
+                    self.resultLabel.text = message
+                    
+                })
+                
             }
+            
+            task.resume()
+            
+        } else {
+            
+            resultLabel.text = "The weather there couldn't be found. Please try again."
+            
         }
         
-        task.resume()
     }
+    
+    override func viewDidLoad() {
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
